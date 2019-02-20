35fcad4568440e7211eed1984a8e5d189dea0c44
Frederick Henderson <frederickjh@henderson-meier.org>
2019-02-20 19:42:32 +0100

    Switched the Drush commands in the script to the long commands as some of the short commands have changed.

--- wbm3cm.sh
+++ wbm3cm.sh
@@ -1,8 +1,8 @@
-drush en -y migrate_run,wbm3cm
-drush mi wbm3cm_step1:node
-drush mi wbm3cm_step2:node
-drush ev 'Drupal\wbm3cm\Wbm3CmConfig::save()'
-drush pmu -y workbench_moderation
-drush en -y content_moderation
-drush ev 'Drupal\wbm3cm\Wbm3CmConfig::restore()'
-drush mi wbm3cm_step3:node
+drush pm:enable -y migrate_run,wbm3cm
+drush migrate:import wbm3cm_step1:node
+drush migrate:import wbm3cm_step2:node
+drush php:eval 'Drupal\wbm3cm\Wbm3CmConfig::save()'
+drush pm:uninstall -y workbench_moderation
+drush pm:enable -y content_moderation
+drush php:eval 'Drupal\wbm3cm\Wbm3CmConfig::restore()'
+drush migrate:import wbm3cm_step3:node
